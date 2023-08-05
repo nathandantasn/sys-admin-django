7f0416561c0aac882b58279021ed7ce81030c430
nathandantasn <than_dantas@hotmail.com>
2023-08-05 19:57:51 -0300

    realizando ultimos ajustes

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from .models import Player, Team, MyTeam
+from .models import Player, Team, MyTeam, Match
 
-admin.site.register(Player)
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_per_page: int = 15
+
+
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ('match_date', 'show_match')
+    list_filter = ('match_date', 'team_a', 'team_b')
+    search_fields = ('team_a__name', 'team_b__name') #lookups
+    def show_match(self, obj: Match):
+        return f'{obj.team_a} {obj.team_a_goal} x {obj.team_b_goal} {obj.team_b}'
+    
+    show_match.short_description = 'Jogo'
+
+admin.site.register(Player, PlayerAdmin)
 admin.site.register(Team)
 admin.site.register(MyTeam)
+admin.site.register(Match, MatchAdmin)
 # Register your models here.
-
--- app/models.py
+++ app/models.py
@@ -3,18 +3,50 @@ from django.db import models
 # Create your models here.
 # Django ORM
 
+
 class Player (models.Model):
-    name = models.CharField(max_length=50)
-    initial_price = models.FloatField()
+    name = models.CharField(max_length=50, verbose_name='Nome')
+    initial_price = models.FloatField(verbose_name='Preço inicial')
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Jogador'
+        verbose_name_plural = 'Jogadores'
 
 class Team (models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name='Nome')
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Time'
+        verbose_name_plural = 'Times'
+
 class MyTeam (models.Model):
-    players = models.ManyToManyField(Player)
+    players = models.ManyToManyField(Player, verbose_name='Jogadores')
+
+    def __str__(self):
+        return [player.name for player in self.players.all()].__str__()
+
+    class Meta:
+        verbose_name = 'Meu Time'
+        verbose_name_plural = 'Meus Times'
+
+class Match(models.Model):
+    match_date = models.DateTimeField(verbose_name='Data do jogo')
+    team_a = models.ForeignKey(Team, on_delete=models.PROTECT,
+                               related_name='team_a_matches', verbose_name='Time A')
+    team_a_goal = models.IntegerField(default=0, verbose_name='Gols do time A')
+    team_b = models.ForeignKey(Team, on_delete=models.PROTECT,
+                               related_name='team_b_matches', verbose_name='Time B')
+    team_b_goal = models.IntegerField(default=0, verbose_name='Gols do time B')
+
+    def __str__(self):
+        return f'{self.team_a} x {self.team_b}'
+    
+    class Meta:
+        verbose_name = 'Jogo'
+        verbose_name_plural = 'Jogos'
